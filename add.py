bd30f3839b09d86cea909e84341bae2f19b10a98
Sachin Awati <awatisachin021@gmail.com>
2025-12-10 11:46:07 +0530

    added addition function

---
+++ add.py
@@ -0,0 +1,10 @@
+class Solution:
+    def add(self, a, b):
+        return a + b
+
+# Example usage:
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.add(2, 3))  # Output: 5
+
+
---
+++ cal.py
@@ -0,0 +1,5 @@
+from add import Solution
+
+sol = Solution()
+result = sol.add(3, 5)
+print("The sum is:", result)
